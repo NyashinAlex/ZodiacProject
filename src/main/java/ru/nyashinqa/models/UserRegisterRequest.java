0be302a29bb8c0c595a9cfe2264abde198e00c87
nyash <alex-nyashin1996@yandex.ru>
2023-10-17 20:02:35 +0300

    Разметка DTO для регистрации нового пользователя (остальное размечу позже)

--- src/main/java/ru/nyashinqa/models/UserRegisterRequest.java
+++ src/main/java/ru/nyashinqa/models/UserRegisterRequest.java
@@ -1,13 +1,18 @@
 package ru.nyashinqa.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Данные пользователя")
 public class UserRegisterRequest {
 
+    @Schema(description = "Имя пользователя")
     private String userName;
+    @Schema(description = "Пароль пользователя")
     private String password;
+    @Schema(description = "Пол пользователя", example = "woman, man")
     private String sex;
 }
